0705546f7a9c67440bc9b2742d0d6b6f62042e23
Ilya Shalyapin <ishalyapin@gmail.com>
2014-04-29 17:20:00 +0600

    fix

---
+++ django_cleanup/__init__.py
@@ -0,0 +1,2 @@
+default_app_config = 'django_cleanup.apps.CleanupConfig'
+
--- django_cleanup/apps.py
+++ django_cleanup/apps.py
@@ -2,10 +2,11 @@
 import django
 if django.VERSION >= (1, 7):
 
-	from django.apps import AppConfig
-	from .models import connect_signals
+    from django.apps import AppConfig
+    from .models import connect_signals
 
-	class CleanupConfig(AppConfig):
+    class CleanupConfig(AppConfig):
+        name = 'django_cleanup'
 
-	    def ready(self):
-	        connect_signals()
+        def ready(self):
+            connect_signals()
