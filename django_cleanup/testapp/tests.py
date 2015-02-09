d1d91f55871825a1fd42180692938d7b39387147
Ilya Shalyapin <ishalyapin@gmail.com>
2015-02-09 12:44:02 +0500

    signals tests

--- django_cleanup/testapp/tests.py
+++ django_cleanup/testapp/tests.py
@@ -2,21 +2,22 @@
 from flexmock import flexmock
 from django.test import TestCase
 from .models import Product
+from django_cleanup.signals import cleanup_pre_delete, cleanup_post_delete
 
 
 class CleanupTest(TestCase):
 
     def test_replace_file(self):
         product = Product.objects.create(image='123.png')
-        flexmock(product.image.storage).should_receive('exists').and_return(True).times(1)
-        flexmock(product.image.storage).should_call('delete').times(1)
+        flexmock(product.image.storage).should_receive('exists').and_return(True).once()
+        flexmock(product.image.storage).should_call('delete').with_args('123.png').once()
         product.image = 'new.png'
         product.save()
 
     def test_remove_model_instance(self):
         product = Product.objects.create(image='123.png')
-        flexmock(product.image.storage).should_receive('exists').and_return(True).times(1)
-        flexmock(product.image.storage).should_call('delete').times(1)
+        flexmock(product.image.storage).should_receive('exists').and_return(True).once()
+        flexmock(product.image.storage).should_call('delete').with_args('123.png').once()
         product.delete()
 
     def test_remove_blank_file(self):
@@ -27,5 +28,14 @@ class CleanupTest(TestCase):
 
     def test_remove_not_exists(self):
         product = Product.objects.create(image='no-such-file.png')
-        flexmock(product.image.storage).should_receive('exists').and_return(False).times(1)
+        flexmock(product.image.storage).should_receive('exists').and_return(False).once()
+        flexmock(product.image.storage).should_call('delete').times(0)
+        product.delete()
+
+    def test_signals(self):
+        product = Product.objects.create(image='123.png')
+        flexmock(product.image.storage).should_receive('exists').and_return(True).once().ordered()
+        flexmock(cleanup_pre_delete).should_call('send').once().ordered()
+        flexmock(product.image.storage).should_call('delete').once().ordered()
+        flexmock(cleanup_post_delete).should_call('send').once().ordered()
         product.delete()
