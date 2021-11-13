c9dfbfbf95816e7e837cb2bfab91a3f8b2e74dea
Mario Rosa <vinnyrose@users.noreply.github.com>
2021-11-13 15:29:54 -0500

    Update __init__.py
    move import to below doc string

--- django_cleanup/__init__.py
+++ django_cleanup/__init__.py
@@ -1,10 +1,10 @@
-import django
-
 '''
     django-cleanup automatically deletes files for FileField, ImageField, and
     subclasses. It will delete old files when a new file is being save and it
     will delete files on model instance deletion.
 '''
+import django
+
 __version__ = '6.0.0-dev'
 if django.VERSION < (3, 2):
     default_app_config = 'django_cleanup.apps.CleanupConfig'
