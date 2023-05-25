2659eaa789dff32efd615751079bd20a4b035425
Mario Rosa <vinnyrose@users.noreply.github.com>
2023-05-25 17:28:50 -0400

    try separate removes

--- test/pytest_plugin/__init__.py
+++ test/pytest_plugin/__init__.py
@@ -9,5 +9,6 @@ def pytest_load_initial_conftests(early_config, parser, args):
     if hasattr(sys, 'gettrace') and sys.gettrace() is not None and '--forked' in args:
         args.remove('--forked')
     if platform.python_implementation() == 'PyPy':
-        args.remove('--cov-report=term-missing --cov=django_cleanup')
+        args.remove('--cov-report=term-missing')
+        args.remove('--cov=django_cleanup')
         args.add('--no-cov')
