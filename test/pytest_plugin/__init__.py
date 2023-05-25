9e1b6ee6e87ba21445371e66e333a6cde1041fb6
Mario Rosa <vinnyrose@users.noreply.github.com>
2023-05-25 17:12:10 -0400

    try removing coverage check for pypy

--- test/pytest_plugin/__init__.py
+++ test/pytest_plugin/__init__.py
@@ -1,4 +1,5 @@
 import sys
+import platform
 
 import pytest
 
@@ -7,3 +8,5 @@ import pytest
 def pytest_load_initial_conftests(early_config, parser, args):
     if hasattr(sys, 'gettrace') and sys.gettrace() is not None and '--forked' in args:
         args.remove('--forked')
+    if platform.python_implementation() == 'PyPy':
+        args.remove('--cov-report=term-missing --cov=django_cleanup')
